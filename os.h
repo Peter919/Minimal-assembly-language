f94a58dd31e6575d8affb8dfed4e4deb13c26dfd
Peter919 <97379969+Peter919@users.noreply.github.com>
2022-06-21 08:44:04 +0200

    Update os.h
    Added unix, so it's not just windows or unknown

--- os.h
+++ os.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #define OS_WINDOWS 0
-#define OS_UNKNOWN 1
+#define OS_UNIX 1
+#define OS_UNKNOWN 2
 
 #ifdef WIN32
 #define OPERATING_SYSTEM OS_WINDOWS
+#elif unix
+#define OPERATING_SYSTEM OS_UNIX
 #else
 #define OPERATING_SYSTEM OS_UNKNOWN
 #endif
