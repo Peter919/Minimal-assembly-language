82369b099375e0e4437e92a6e9fb898ce7a19c0e
Peter919 <97379969+Peter919@users.noreply.github.com>
2022-06-21 08:37:56 +0200

    Update settings.c
    Added g_pause_seconds_after_print so execution can be paused while the user looks at what has been printed

--- settings.c
+++ settings.c
@@ -21,7 +21,10 @@ char * g_builtin_identifier_names[] = {"vpt", "ipt", "in", "out", "one", ""};
 char * g_label_keyword = "lab";
 
 // primary can be used at the beginning of the name
-char * g_identifier_primary_symbols = "abcdefghijklmnnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_";
-char * g_identifier_secondary_symbols = "-0123456789";
+char * g_identifier_primary_symbols = "abcdefghijklmnnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
+char * g_identifier_secondary_symbols = "0123456789";
 
-const float g_print_delay_seconds = 0;
+// the screen must be cleared between each call of print
+// so it shouldn't be executed after every single instruction
+const float g_seconds_between_printings = 1;
+const float g_pause_seconds_after_print = 0;
