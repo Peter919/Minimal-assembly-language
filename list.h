c4dee4bf55caca202b3ae961f0f4f350f54cdd93
Peter919 <97379969+Peter919@users.noreply.github.com>
2022-06-25 18:03:59 +0200

    Update list.h
    Added list_combine()

--- list.h
+++ list.h
@@ -22,6 +22,8 @@ char list_insert_after_node(struct Node * node, void * value);
 
 char list_pop(struct List * list, unsigned int index, void (* free_value)(void *));
 
+void list_combine(struct List * dest, struct List * src);
+
 void list_delete(struct List * list, void (*free_value)(void *));
 
 void log_list(enum e_LogLevel log_level, struct List * list, void (* node_print_function)(enum e_LogLevel, void *), char * separator);
